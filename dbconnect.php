17e77cc0180a21b832c8903b607ca2bafc9c68f2
nedh <nedh.design@gmail.com>
2022-12-02 10:34:55 +0100

    modifié paramètres DB pour upload sur Plesk

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,9 @@
                                         <!--DBCONNECT BLOC-->
 <?php // connexion à la base de données
-        $conn = mysqli_connect('localhost', 'root', '', 'moduleconnexion');
+
+/*         $conn = mysqli_connect('localhost', 'root', '', 'moduleconnexion'); */
+        $conn = mysqli_connect('localhost', 'nadia', '*moduleconnexion*', 'nadia-hazem_moduleconnexion');
+
         if(!$conn) {
             echo "Connexion non établie.";
             exit;
---
+++ moduleconnexion.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 02 déc. 2022 à 09:18
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `moduleconnexion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `prenom`, `nom`, `password`) VALUES
+(13, 'admin', 'admin', 'admin', '$2y$10$v/MXWuSzvPS4NOP2.x2mvub7Mz2XaA8Kbz3s3H/XqaZfc35k6.J7e'),
+(12, 'jon', 'john', 'doe', '$2y$10$t99lNaxxGf0I8yxwp4Epse6spa7JNV4eN.tZNWCzhdkiifdol0OUu');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
